35a141d30935603182d5e49c968d4ebfcaf1a6f8
Veggietail8 <thebritainbiscuit@gmail.com>
2025-09-27 09:35:15 -0700

    Add basic JS code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input-element");
+  let cityElement = document.querySelector("h1.weather-app-city");
+  cityElement.innerHTML = searchInput.value;
+}
 
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
