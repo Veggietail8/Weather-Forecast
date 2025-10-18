64388ecd8c5fe9e076332a4a4a97292e11680b68
Veggietail8 <thebritainbiscuit@gmail.com>
2025-10-18 10:48:21 -0700

    Removing dummy data

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ function refreshTime() {
   let currentHour = now.getHours();
   let currentMinute = now.getMinutes();
   console.log(day, currentHour, currentMinute);
+
+  // Format the minutes when it's less than 10
+  if (currentMinute < 10) {
+    currentMinute = `0${currentMinute}`;
+  }
+
   let dayElement = document.querySelector("#current-day");
   let hourElement = document.querySelector("#current-hour");
   let minuteElement = document.querySelector("#current-minute");
@@ -22,31 +28,43 @@ function refreshTime() {
 }
 
 function refreshWeather(response) {
-  console.log(response.data);
+  // Update the current temperature
   let tempNumberElement = document.querySelector("#weather-app-temp-number");
+  let currentTemp = response.data.temperature.current;
+  tempNumberElement.innerHTML =
+    Math.round(currentTemp); /*rounding up the temperature*/
+
+  // Update the city name
+  let cityElement = document.querySelector("h1.weather-app-city");
+  cityElement.innerHTML =
+    response.data.city; /*Displaying the city name as how it's shown in the API, not how it's typed by the user*/
+
+  // Update the humidity
   let humidityPercentElement = document.querySelector("#humidity-number");
+  let currentHumidity = response.data.temperature.humidity;
+  humidityPercentElement.innerHTML = `${currentHumidity}%`;
+
+  // Update the wind speed
   let windSpeedElement = document.querySelector("#wind-number");
+  let currentWind = response.data.wind.speed;
+  windSpeedElement.innerHTML = `${currentWind}km/h`;
+
+  // Update the weather condition description
   let weatherConditionElement = document.querySelector(
     "#weather-condition-description"
   );
-  let weatherIconElement = document.querySelector("#weather-app-temp-icon");
-
-  let currentTemp = response.data.temperature.current;
-  let currentHumidity = response.data.temperature.humidity;
-  let currentWind = response.data.wind.speed;
   let currentWeatherCondition = response.data.condition.description;
-  let currentWeatherIconUrl = response.data.condition.icon_url;
-  console.log(currentWeatherIconUrl);
-
-  tempNumberElement.innerHTML = currentTemp;
-  humidityPercentElement.innerHTML = `${currentHumidity}%`;
-  windSpeedElement.innerHTML = `${currentWind}km/h`;
   weatherConditionElement.innerHTML = currentWeatherCondition;
+
+  // Update the weather icon
+  let weatherIconElement = document.querySelector("#weather-app-temp-icon");
+  let currentWeatherIconUrl = response.data.condition.icon_url;
   weatherIconElement.innerHTML = `<img src=${currentWeatherIconUrl} alt="Icon"></img>`;
 
   refreshTime();
 }
 
+//Receiving the search input value and look for the city-specific API; Call the API, and trigger the function which refreshes data on the UI
 function searchCity(city) {
   let apiKey = "836444o057f4ft35027a7882a9606bb0";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -56,10 +74,10 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input-element");
-  let cityElement = document.querySelector("h1.weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Vancouver"); /*Display a default city when the page is refreshed*/
